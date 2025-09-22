9c9caa96794fe9c674919f783dc245c5d405ad72
Jared <69952904+jaredatandi@users.noreply.github.com>
2025-09-23 01:08:32 +0300

    Update firebase.ts
    hold on

--- subtitleo-client-master/subtitleo-client-master/src/firebase.ts
+++ subtitleo-client-master/subtitleo-client-master/src/firebase.ts
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFUp_sum9blanxteoYGaxyapvHekEZQZo",
+  apiKey: "secret",
   authDomain: "subtitleo.firebaseapp.com",
   projectId: "subtitleo",
   storageBucket: "subtitleo.appspot.com",
